9e1b12d3a201a5caacdfb9fe5416cd57c46674e2
Rajdeep <rajdeep9rdm@gmail.com>
2021-10-01 01:07:13 +0530

    deleted un necessary files

--- Hacktoberfest-imagine-your-contribution-main/Hacktoberfest-imagine-your-contribution-main/Python programs/dice.py
+++
@@ -1,8 +0,0 @@
-
-import random
-roll_again = "yes"
-
-while(roll_again == "yes" or roll_again == "y"):
-
-    print(random.randint(1, 7))
-    roll_again = input("Do you want to roll again: y/yes or n/no:: ")
